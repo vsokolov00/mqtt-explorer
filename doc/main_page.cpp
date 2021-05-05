66bcc6cd97d2f712de042727acc8d447237b4aad
David mihola <mihola.dejv@gmail.com>
2021-05-05 14:20:32 +0200

    main page

---
+++ doc/main_page.cpp
@@ -0,0 +1,12 @@
+/**
+ * @mainpage MQTT Explorer and Simulator
+ * @brief Applications implemented within the The C++ Programming Language course at the Brno University of Technology.
+ * @author David Mihola, xmihol00@stud.fit.vutbr.cz
+ * @author Vladislav Sokolovskii, xsokol15@stud.fit.vutbr.cz
+ *  
+ * @section S1 User Interface Preview
+ * 
+ * \image html images/login_window.png "The login window of the MQTT Explorer." <br>
+ * 
+ * \image html images/main_window.png "The main window of the MQTT Explorer."
+ **/
