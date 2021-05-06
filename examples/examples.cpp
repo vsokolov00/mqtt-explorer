6a53c6ee58db5ecd6ef78c858f3ffd5e469f86e6
David mihola <mihola.dejv@gmail.com>
2021-05-06 14:48:43 +0200

    template files

--- examples/examples.cpp
+++ examples/examples.cpp
@@ -1,9 +1,19 @@
+//========================================================================================
+// File:        examples.cpp
+// Case:        VUT, FIT, ICP, project                             
+// Author:      David Mihola, xmihol00@stud.fit.vutbr.cz           
+// Date:        summer semester 2021                               
+// Brief:       Documentation of the mainpage of the generated HTML output with Doxygen.
+//========================================================================================
+
 /**
  * @mainpage MQTT Explorer and Simulator
  * @brief Applications implemented within The C++ Programming Language course at the Brno University of Technology.
  * @author David Mihola, xmihol00@stud.fit.vutbr.cz
  * @author Vladislav Sokolovskii, xsokol15@stud.fit.vutbr.cz
- *  
+ * 
+ * @tableofcontents
+ * 
  * @section S1 User Interface Preview
  * 
  * \image html ../examples/images/login_window.png "The login window of the MQTT Explorer." <br>
@@ -17,4 +27,11 @@
  * \image html ../examples/images/main_window_example.png "Example how to use the main window of the application." <br>
  * 
  * \image html ../examples/images/history_example.png "Example how to view a history of a topic and a detail of a specific message." <br>
+ * 
+ * @section S3 Examples of Usage of The Simulator
+ * @subsection ss1 Template JSON file for simulated devices configuration.
+ * @verbinclude ../examples/simulator_template.json <br>
+ * 
+ * @subsection ss2 Template JSON file for sending a message to a device.
+ * @verbinclude ../examples/message_template.json <br>
  **/
